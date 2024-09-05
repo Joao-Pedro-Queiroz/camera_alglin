41b42fa299b176732ba43250cf15dbb1b7b81d9d
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-09-05 12:29:14 -0300

    projeto quase pronto

--- camera_alglin/demo.py
+++ camera_alglin/demo.py
@@ -1,6 +1,14 @@
 import numpy as np
 import cv2 as cv
 
+def criar_indices(min_i, max_i, min_j, max_j):
+    import itertools
+    L = list(itertools.product(range(min_i, max_i), range(min_j, max_j)))
+    idx_i = np.array([e[0] for e in L])
+    idx_j = np.array([e[1] for e in L])
+    idx = np.vstack( (idx_i, idx_j) )
+    return idx
+
 
 def run():
     # Essa função abre a câmera. Depois desta linha, a luz de câmera (se seu computador tiver) deve ligar.
@@ -14,7 +22,7 @@ def run():
     #Matrizes de transformação
     T = np.array([[1, 0, height/2], [0, 1, width/2], [0, 0, 1]])
     R = np.array([[0.7, -0.7, 0], [0.7, 0.7, 0], [0, 0, 1]])
-    X = np.linalg.inv(T) @ R @ T
+    Y = np.linalg.inv(T) @ R @ T
 
     # Talvez o programa não consiga abrir a câmera. Verifique se há outros dispositivos acessando sua câmera!
     if not cap.isOpened():
@@ -34,18 +42,26 @@ def run():
         # Mudo o tamanho do meu frame para reduzir o processamento necessário
         # nas próximas etapas
         frame = cv.resize(frame, (width,height), interpolation =cv.INTER_AREA)
-        frame_trnsformado = X @ frame
-        frame_trnsformado = frame_trnsformado.astype(int)
-
-        # Troque este código pelo seu código de filtragem de pixels
-        frame_trnsformado[0,:] = np.clip(frame_trnsformado[0,:], 0, frame.shape[0])
-        frame_trnsformado[1,:] = np.clip(frame_trnsformado[1,:], 0, frame.shape[1])
 
         # A variável image é um np.array com shape=(width, height, colors)
-        image = np.array(frame_trnsformado).astype(float)/255
+        image = np.array(frame).astype(float)/255
+        image_ = np.zeros_like(image)
+
+        X = criar_indices(0, width, 0, height)
+        X = np.vstack ( (X, np.ones( X.shape[1]) ) )
+
+        Xd = Y @ X
+        Xd = Xd.astype(int)
+        X = X.astype(int)
+
+        # Troque este código pelo seu código de filtragem de pixels
+        Xd[0,:] = np.clip(Xd[0,:], 0, image.shape[0])
+        Xd[1,:] = np.clip(Xd[1,:], 0, image.shape[1])
+
+        image_[Xd[0,:], Xd[1,:], :] = image[X[0,:], X[1,:], :]
 
         # Agora, mostrar a imagem na tela!
-        cv.imshow('Minha Imagem!', image)
+        cv.imshow('Minha Imagem!', image_)
         
         # Se aperto 'q', encerro o loop
         if cv.waitKey(1) == ord('q'):
