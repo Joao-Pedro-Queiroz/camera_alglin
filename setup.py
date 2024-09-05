578f3af4ce9d168046d8a6b865d1dc29c8b02397
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2024-09-05 11:45:57 -0300

    melhorando setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,17 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],
-    author="João Pedro Queiroz Viana, Pedro Fardin",
+    author="João Pedro Queiroz Viana, Felipe Mariano",
     author_email="joaopqv@al.insper.edu.br, ",
     description="Uma biblioteca de criptografia.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Joao-Pedro-Queiroz/enigma",
+    entry_points={
+        'console_scripts': [
+            'camera_alglin=camera_alglin.demo:main',
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
